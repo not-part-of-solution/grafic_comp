0436f65a8c133d5d5d9814d3a01ecfb47e2f7770
not-part-of-solution <aeztva@gmail.com>
2025-02-13 12:36:21 +0400

    LABA_1. Task 4. Ex_1

--- PythonProject/Number_4.0/Ex_4.py
+++ PythonProject/Number_4.0/Ex_4.py
@@ -1,7 +1,8 @@
+from PIL import Image, ImageOps
+
 def pars_for_me(file_path):
 
     arr_vertex = []
-    arr_f = []
     with open(file_path, 'r') as objFile:
         for line in objFile:
             parts = line.strip().split()
@@ -11,17 +12,29 @@ def pars_for_me(file_path):
             if type == "v":
                 x, y, z = map(float, parts[1:4])
                 arr_vertex.append((x, y, z))
-            elif type == "f":
-                if len(parts) > 1:
-                    arr_f.append(([int(part.split('/')[0]) for part in parts[1:]])) #int
 
-    return arr_vertex, arr_f
+    return arr_vertex
+
+def draw_vertex(arr, image_size = (1000,1000), mult = 2000, up_down= 500): #рисуем вершинки
+    image = Image.new("RGB", image_size, "white")
+    pixel = image.load()
+    color = (255, 0, 0)
+    for vertex in arr:
+        x, y, z = vertex
+        x_pixel = int(mult * x + up_down)  #масштабируем
+        y_pixel = int(mult * y + up_down)
+        pixel[x_pixel, y_pixel] = color
+
+    return image
+
 
 def main():
     file_path = r'model_1.obj'
-    arr_f = pars_for_me(file_path)
-    for i in range(0, len(arr_f)):
-        print(arr_f[i])
+    arr = pars_for_me(file_path)
+    image = draw_vertex(arr)
+    image = ImageOps.flip(image) #повернули зайца
+    image.save('Caraculi.png')
+    image.show()
 
 if __name__ == "__main__":
     main()
